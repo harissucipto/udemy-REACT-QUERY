ad6e43cf566fd2b82103e13ebb30622e91a45808
haris sucipto <harisssucipto@gmail.com>
2021-09-30 01:55:32 +0700

    adding devtools

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,4 +1,5 @@
 import { Posts } from "./Posts";
+import { ReactQueryDevtools } from "react-query/devtools";
 import "./App.css";
 
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -14,6 +15,7 @@ function App() {
         <h1>Blog Posts</h1>
         <Posts />
       </div>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
