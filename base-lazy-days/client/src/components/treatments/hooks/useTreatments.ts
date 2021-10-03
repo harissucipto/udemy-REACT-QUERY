24fcc1d11faecc48512f9dd8f64c913ce27ebe51
haris sucipto <harisssucipto@gmail.com>
2021-10-03 09:14:45 +0700

    implment global erro rhandling

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,6 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -12,18 +11,7 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
-  const toast = useCustomToast();
-
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
-    onError: (error) => {
-      // checking erro type
-      const title =
-        error instanceof Error
-          ? error.toString().replace(/^Error:\s*/, '')
-          : 'error connecting to the server';
-      toast({ title, status: 'error' });
-    },
-  });
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
   return data;
 }
